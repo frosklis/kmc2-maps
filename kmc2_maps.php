4e58acac88cf10f4c5d8fcf12c65b0322b706b78
Claudio Noguera <claudio.noguera@gmail.com>
2013-07-24 21:00:35 +0200

    Esqueleto

---
+++ kmc2_maps.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Km C2 maps
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: The maps that appear on the kmc2 website
+Version: 0.1
+Author: Claudio Noguera
+Author URI: http://claudionoguera.tk/blog
+License: A "Slug" license name e.g. GPL2
+*/
+?>

be35a55f14b540a84dcf74d97cb79bef69a09738
Claudio Noguera <claudio.noguera@gmail.com>
2014-02-09 22:22:35 +0100

    add files

---
+++ js/kmc2-trips-visualization.js
@@ -0,0 +1,17 @@
+/*global jQuery: false, L:false*/
+/*jslint bitwise: true*/
+jQuery(document).ready(function () {
+    'use strict';
+    var map, layer;
+    map = L.map('trips-visualization-map');
+
+    // Using stame map
+    // replace "toner" here with "terrain" or "watercolor"
+    layer = new L.StamenTileLayer("watercolor");
+    map.addLayer(layer);
+
+	map.setView(L.latLng(30, 0), 2);
+
+
+	map.locate({setView: true, maxZoom: 16});
+});

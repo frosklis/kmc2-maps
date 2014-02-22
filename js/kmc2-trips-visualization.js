2787acb18c4b001bec92edfc3a853b3218451183
Claudio Noguera <claudio.noguera@gmail.com>
2014-02-22 20:18:18 +0100

    add street lines if a certain zoom level is reached

--- js/kmc2-trips-visualization.js
+++ js/kmc2-trips-visualization.js
@@ -2,7 +2,7 @@
 /*jslint bitwise: true*/
 jQuery(document).ready(function () {
     'use strict';
-    var map, layer, markers, marker, i, a, title, ajaxUrl;
+    var map, layer, markers, marker, i, a, title, ajaxUrl, Stamen_TonerLabels;
 
     ajaxUrl = kmc2_visualization_vars.siteurl + 'wp-admin/admin-ajax.php';
 
@@ -14,21 +14,22 @@ jQuery(document).ready(function () {
     map = L.map('trips-visualization-map',
 		{
 			'scrollWheelZoom': false,
-			'doubleClickZoom': false,
+			// 'doubleClickZoom': false,
 			'touchZoom': false,
             'fullscreenControl': true
 		});
     map.on ('enterFullscreen', function() {
         map.scrollWheelZoom.enable();
-        map.doubleClickZoom.enable();
+        // map.doubleClickZoom.enable();
         map.touchZoom.enable();
     });
 
     map.on ('exitFullscreen', function() {
         map.scrollWheelZoom.disable();
-        map.doubleClickZoom.disable();
+        // map.doubleClickZoom.disable();
         map.touchZoom.disable();
     });
+
 
     // Using stame map
     // replace "toner" here with "terrain" or "watercolor"
@@ -37,6 +38,13 @@ jQuery(document).ready(function () {
 
 	map.setView(L.latLng(30, 0), 2);
 
+    Stamen_TonerLabels = L.tileLayer('http://{s}.tile.stamen.com/toner-labels/{z}/{x}/{y}.png', {
+        // attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        subdomains: 'abcd',
+        minZoom: 8,
+        maxZoom: 20
+    });
+    map.addLayer(Stamen_TonerLabels);
 
     // Add clusters of posts and pictures!
     markers = L.markerClusterGroup(
